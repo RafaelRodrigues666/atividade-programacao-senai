12ceceda8ebbb2bbcdd2606917b4d6281f367bf7
RafaelRodrigues666 <patriciaoliveira.po270@gmail.com>
2025-02-17 17:04:06 -0300

    atividade-programacao-senai

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Digite um Número: ");
-        int numero = entrada.nextInt();
+        System.out.println("Digite seu nome: ");
+        String nome = entrada.nextLine();
 
-        if(numero >= 10 && numero <= 50){
-            if(numero % 2 == 0){
-                System.out.println(numero +" é par, e está entre 10 e 50!");
-            }else{
-                System.out.println(numero +" é ímpar, e está entre 10 e 50!");
-            }
+        System.out.println("Digite sua Idade: ");
+        int idade = entrada.nextInt();
+
+        System.out.println("Seu acesso (1 para Admin, 2 para Usuário): ");
+        int acesso = entrada.nextInt();
+
+        if(idade >= 18 || acesso == 1){
+            System.out.println("Acesso Permitido");
+            System.out.println("Nome: " +nome);
+            System.out.println("Idade: " +idade);
+            System.out.println("Admin: " +acesso);
         }else{
-            System.out.println("Seu número não está entre 10 e 50!! Tente Novamente.");
+            System.out.println("Acesso Negado");
+            System.out.println("Necessário 18 anos ou ter acesso Admin!");
         }
     }
 }
