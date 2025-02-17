a05d946e79e4d5ebd6652753f5ff150ce4c615ad
RafaelRodrigues666 <patriciaoliveira.po270@gmail.com>
2025-02-17 17:06:14 -0300

    atividade-programacao-senai

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Digite um Número: ");
-        int numero = entrada.nextInt();
+        System.out.println("Digite sua idade: ");
+        int idade = entrada.nextInt();
 
-        if (numero >= 20 && numero <= 90) {
-            System.out.println("Seu número está no intervalo (20 a 90): " +numero);
-        }else {
-            System.out.println("Seu numero não está entre (20 a 90) Tente Novamente.");
+        System.out.println("Possui CNH (1 para sim, 0 para não): ");
+        int cnh = entrada.nextInt();
+
+        if(idade >= 18 && cnh == 1){
+            System.out.println("Verificação Aprovada!!");
+        }else{
+            System.out.println("Verificação Recusada!! Necessário +18 e CNH!");
         }
+
     }
 }
